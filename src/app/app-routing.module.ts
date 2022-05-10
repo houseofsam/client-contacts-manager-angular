f81ebcf661b5b3d9411be256e38d1485274420c1
houseofsam <samxsaid@gmail.com>
2022-05-10 14:02:36 -0400

    Create company-page component, add it to company module, and add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientPageComponent } from './clients/client-page/client-page.component';
+import { CompanyPageComponent } from './company/company-page/company-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -8,9 +10,22 @@ const routes: Routes = [
     component: ClientPageComponent,
   },
   {
+    path: "client",
+    redirectTo: "/clients",
+    pathMatch: "full",
+  },
+  {
+    path: "company",
+    component: CompanyPageComponent,
+  },
+  {
     path: '',
     redirectTo: "/clients",
     pathMatch: "full",
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AdminModuleModule } from './admin/admin-module/admin-module.module';
 import { ClientModule } from './clients/client.module';
-import { CompanyModule } from './company/company/company.module';
+import { CompanyModule } from './company/company.module';
 import { SharedModule } from './shared/shared/shared.module';
 import { CustomMaterialModule } from './custom-material/custom-material.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
