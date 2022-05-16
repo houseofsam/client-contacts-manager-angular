39da3f9e100c6a46473b4638a20620795cd3e198
houseofsam <samxsaid@gmail.com>
2022-05-16 12:35:27 -0400

    Create Client class interface and add API call methods to client.service

--- src/app/clients/client.service.ts
+++ src/app/clients/client.service.ts
@@ -1,14 +1,54 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Client } from './client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
+  private readonly clientAPIUrl = 'api/clients';
 
   constructor(private httpClient: HttpClient) { }
 
   log (Text: string) {
     console.log(Text);
+  }
+
+  // save client to local storage
+  save(client: Client) {
+    const url = `${this.clientAPIUrl}`;
+    return this.httpClient.post(url, client);
+  }
+
+  // get client by clientID
+  getClientById(clientId: string): any {
+    const url = `${this.clientAPIUrl}/${clientId}`;
+    return this.httpClient.get(url);
+  }
+
+  // get list of clients
+  getClients(): any {
+    const url = `${this.clientAPIUrl}`;
+    return this.httpClient.get(url);
+  }
+
+  // archive a client
+  deleteClient(clientId: string): any {
+    const url = `${this.clientAPIUrl}/${clientId}`;
+    return this.httpClient.delete(url);
+  }
+
+  // update a clients details
+  updateClient(client: Client): any {
+    const url = `${this.clientAPIUrl}`;
+    return this.httpClient.put(url, client);
+  }
+
+  // search the clients list
+  search(searchTerm: string): any {
+    searchTerm.trim();
+    const url = `${this.clientAPIUrl}`;
+    const searchOptions = searchTerm ? {params: new HttpParams().set('firstname', searchTerm)} : {};
+    return this.httpClient.get(url, searchOptions);
   }
 }
